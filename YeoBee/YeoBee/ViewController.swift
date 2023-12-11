78c4261938cba12f3f111fabb972a3fa1c4faf79
llghdud921 <llghdud921@naver.com>
2023-12-12 00:56:18 +0900

    🎉 프로젝트 생성

---
+++ YeoBee/YeoBee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YeoBee
+//
+//  Created by 이호영 on 2023/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
