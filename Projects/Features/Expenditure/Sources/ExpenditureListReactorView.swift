abf42b9029e409216eb48c30b304c1c68961f45e
Jerry_hoyoung <llghdud921@naver.com>
2023-12-26 15:24:06 +0900

    ✨ 지출 내역 리스트 View, Reactor 추가

---
+++ Projects/Features/Expenditure/Sources/ExpenditureListReactorView.swift
@@ -0,0 +1,48 @@
+//
+//  ExpenditureListReactorView.swift
+//  Expenditure
+//
+//  Created by Hoyoung Lee on 12/26/23.
+//  Copyright © 2023 YeoBee.com. All rights reserved.
+//
+
+import Foundation
+import ReactorKit
+import RxSwift
+
+import DesignSystem
+import Snapkit
+
+class ExpenditureListReactorView: Reactor {
+    
+    enum Action {
+        // actiom cases
+    }
+    
+    enum Mutation {
+        // mutation cases
+    }
+    
+    struct State {
+        //state
+    }
+    
+    let initialState: State
+    
+    init() {
+        // init state initialState = State(...)
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        // switch action {
+        // }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        // switch mutation {
+        // }
+        return newState
+    }
+    
+}
