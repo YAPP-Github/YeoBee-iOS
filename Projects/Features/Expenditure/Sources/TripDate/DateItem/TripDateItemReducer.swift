2eb5574e4e58fbe7e304a1cd0fa42520ba7ececf
Jerry_hoyoung <llghdud921@naver.com>
2023-12-30 21:04:45 +0900

    🐛 TripDateItemReducer 추가

---
+++ Projects/Features/Expenditure/Sources/TripDate/DateItem/TripDateItemReducer.swift
@@ -0,0 +1,42 @@
+//
+//  TripDateItemReducer.swift
+//  Expenditure
+//
+//  Created Hoyoung Lee on 12/29/23.
+//  Copyright © 2023 YeoBee.com. All rights reserved.
+
+import Foundation
+import ComposableArchitecture
+
+public struct TripDateItemReducer: Reducer {
+    public struct State: Equatable, Identifiable {
+        public var id: TimeInterval { date.timeIntervalSince1970 }
+        var isSelected: Bool
+        var week: String
+        var day: Int
+        var date: Date
+
+        init(isSelected: Bool, date: Date) {
+            self.isSelected = isSelected
+            self.date = date
+
+            let calendar = Calendar.current
+            let dayNumber = calendar.component(.weekday, from: date)
+            let daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"]
+            self.week = daysOfWeek[dayNumber - 1]
+            self.day = calendar.component(.day, from: date)
+        }
+    }
+
+    public enum Action {
+        case tappedItem
+    }
+    
+    public var body: some ReducerOf<TripDateItemReducer> {
+        Reduce { _, action in
+            switch action {
+                default: return .none
+            }
+        }
+    }
+}
