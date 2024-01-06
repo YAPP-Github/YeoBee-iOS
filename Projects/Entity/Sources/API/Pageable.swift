2bcd7b9eaf8aa21adda498d5d72a10c9314b2422
taetae <taetae.dev@gmail.com>
2024-01-06 16:29:17 +0900

    Pageable 모델 타입 추가

---
+++ Projects/Entity/Sources/API/Pageable.swift
@@ -0,0 +1,19 @@
+//
+//  Pageable.swift
+//  Entity
+//
+//  Created by 태태's MacBook on 1/6/24.
+//  Copyright © 2024 YeoBee.com. All rights reserved.
+//
+
+import Foundation
+
+// 페이지 정보
+struct Pageable: Codable {
+    var pageNumber: Int
+    var pageSize: Int
+    var sort: SortType
+    var offset: Int
+    var paged: Bool
+    var unpaged: Bool
+}
