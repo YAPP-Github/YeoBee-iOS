70f8134e068ad97da35156c306420026ee473d56
taetae <taetae.dev@gmail.com>
2024-01-06 16:29:48 +0900

    Sort Type 추가

---
+++ Projects/Entity/Sources/API/SortType.swift
@@ -0,0 +1,16 @@
+//
+//  SortType.swift
+//  Entity
+//
+//  Created by 태태's MacBook on 1/6/24.
+//  Copyright © 2024 YeoBee.com. All rights reserved.
+//
+
+import Foundation
+
+//정렬 기준에 대한 모델
+struct SortType: Codable {
+    var empty: Bool
+    var unsorted: Bool
+    var sorted: Bool
+}
