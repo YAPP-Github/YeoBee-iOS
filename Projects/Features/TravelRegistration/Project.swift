e321a10db2348261d16307ccf15cbd9b354de219
Hyeonjoonn <phj3046@gmail.com>
2024-01-06 14:55:21 +0900

    ✨ 여행등록 피쳐 FSCalendar 추가

--- Projects/Features/TravelRegistration/Project.swift
+++ Projects/Features/TravelRegistration/Project.swift
@@ -22,7 +22,8 @@ let project = Project(
                 .RxSwift,
                 .RxCocoa,
                 .RxGesture,
-                .reactorKit
+                .reactorKit,
+                .fscalendar
             ]
         ),
         Project.target(
