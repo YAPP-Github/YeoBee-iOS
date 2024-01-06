ccf51e5ec8061450a13bc9e4924bc713570db1f4
taetae <taetae.dev@gmail.com>
2024-01-06 16:39:54 +0900

    PaymentMethod enum 타입 추가

---
+++ Projects/Entity/Sources/API/Expense/PaymentMethod.swift
@@ -0,0 +1,15 @@
+//
+//  PaymentMethod.swift
+//  Entity
+//
+//  Created by 태태's MacBook on 1/5/24.
+//  Copyright © 2024 YeoBee.com. All rights reserved.
+//
+
+import Foundation
+
+// 결제 방식을 위한 Enum
+enum PaymentMethod: String, Codable {
+    case cash = "CASH"
+    case card = "CARD"
+}
