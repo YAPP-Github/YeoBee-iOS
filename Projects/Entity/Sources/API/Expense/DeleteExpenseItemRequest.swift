c4a1c94b966adfe6de8d09a824b26341e63306c9
taetae <taetae.dev@gmail.com>
2024-01-06 16:39:24 +0900

    Delete Expense Item Request 추가

---
+++ Projects/Entity/Sources/API/Expense/DeleteExpenseItemRequest.swift
@@ -0,0 +1,13 @@
+//
+//  DeleteExpenseItemRequest.swift
+//  Entity
+//
+//  Created by 태태's MacBook on 1/5/24.
+//  Copyright © 2024 YeoBee.com. All rights reserved.
+//
+
+import Foundation
+
+struct DeleteExpenseItemRequest: Codable {
+    var expenseId: String
+}
