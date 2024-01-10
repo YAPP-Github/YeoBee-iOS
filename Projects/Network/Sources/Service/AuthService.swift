757c3a82dec4b47ca75f63c7bca7251d3fb06bd4
taetae <taetae.dev@gmail.com>
2024-01-11 00:38:49 +0900

    AuthService 추가 (카카오)

---
+++ Projects/Network/Sources/Service/AuthService.swift
@@ -0,0 +1,44 @@
+//
+//  AuthService.swift
+//  Network
+//
+//  Created by 태태's MacBook on 1/2/24.
+//  Copyright © 2024 YeoBee.com. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+enum AuthService {
+    case kakaoLogin(token: String)
+}
+
+extension AuthService: TargetType {
+    var baseURL: URL { return URL(string: BaseURL.string)!}
+    
+    var path: String {
+        switch self {
+        case .kakaoLogin:
+            return "/v1/auth/kakao"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .kakaoLogin:
+            return .post
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .kakaoLogin(let token):
+            return .requestParameters(parameters: ["oauthToken": token], encoding: JSONEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return ["Content-type": "application/json"]
+    }
+    
+}
