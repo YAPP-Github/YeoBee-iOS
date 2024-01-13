5c9909a82f4ad81e81a05427723be3cdc307b643
Taetae <taetae.dev@gmail.com>
2024-01-13 20:03:13 +0900

    [YB-74]네트워크- 로그인 쪽 기본 틀  (#23)
    * BaseURL 파일 git ignore 추가
    
    * AuthService 추가 (카카오)
    
    * 로그인 관련 Repository 추가
    
    * 지출 관련 Expense Service 추가
    
    * 피드백 반영
    - 옵셔널 바인딩 간소화
    - 불필요한 공백, 개행 제거
    
    * 피드백 반영

---
+++ Projects/Network/Sources/Repository/AuthRepository.swift
@@ -0,0 +1,34 @@
+//
+//  AuthRepository.swift
+//  Network
+//
+//  Created by 태태's MacBook on 1/2/24.
+//  Copyright © 2024 YeoBee.com. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+protocol AuthRepositoryInterface {
+    func loginWithKakao(token: String)
+    func loginWithApple()
+    func logout()
+}
+
+final class AuthRepository: AuthRepositoryInterface {
+    let provider = MoyaProvider<AuthService>()
+    
+    func loginWithKakao(token: String) {
+        provider.request(.kakaoLogin(token: token)) { result in
+            // 세부구현
+        }
+    }
+    
+    func loginWithApple() {
+        // apple login
+    }
+    
+    func logout() {
+        // logout 기능
+    }
+}
