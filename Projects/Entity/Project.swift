32ba2ca13e421c42d294507a41c516f09f3f42c8
Taetae <taetae.dev@gmail.com>
2024-01-15 00:46:36 +0900

    [Fix] Entity의 Project 파일 추가 (#26)
    * Entity 의 Project 파일 추가
    
    * 불필요 의존성 삭제

---
+++ Projects/Entity/Project.swift
@@ -0,0 +1,36 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project(
+    name: "Entity",
+    organizationName: "YeoBee.com",
+    options: .options(
+        automaticSchemesOptions: .disabled
+    ),
+    packages: [],
+    settings: nil,
+    targets: [
+        Project.target(
+            name: "Entity",
+            product: .framework,
+            sources: "Sources/**",
+            dependencies: []
+        )
+    ],
+    schemes: [
+        Scheme(
+            name: "Entity",
+            shared: true,
+            buildAction: BuildAction(
+                targets: ["Entity"]
+            ),
+            runAction: .runAction(configuration: .debug),
+            archiveAction: .archiveAction(configuration: .debug),
+            profileAction: .profileAction(configuration: .debug),
+            analyzeAction: .analyzeAction(configuration: .debug)
+        )
+    ], 
+    fileHeaderTemplate: nil,
+    additionalFiles: [],
+    resourceSynthesizers: []
+)
