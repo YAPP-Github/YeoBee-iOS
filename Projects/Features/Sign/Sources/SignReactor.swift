8bf1f5638667d608f8dfb27ddedf9da97950991a
taeHyeong Kim <taetae.dev@gmail.com>
2024-02-10 15:54:57 +0900

    feat: 카카오 로그인 구현 (토큰 분기 추가 필요)

---
+++ Projects/Features/Sign/Sources/SignReactor.swift
@@ -0,0 +1,115 @@
+//
+//  SignReactor.swift
+//  Sign
+//
+//  Created by 김태형 on 2/5/24.
+//  Copyright © 2024 YeoBee.com. All rights reserved.
+//
+
+import Foundation
+import ReactorKit
+import RxSwift
+
+import KakaoSDKAuth
+import KakaoSDKUser
+import Repository
+
+import YBNetwork
+
+public final class SignReactor: Reactor {
+    public enum Action  {
+        case kakao
+        case apple
+    }
+    
+    public enum Mutation {
+        case setLoginStatus(Bool)
+    }
+    
+    public struct State {
+        var isLoginSuccess: Bool
+    }
+    
+    public let initialState: State
+    
+    public init(
+        isLoginSuccess: Bool
+    ) {
+        self.initialState = .init(
+            isLoginSuccess: isLoginSuccess
+        )
+    }
+    
+    let authRepository = AuthRepository()
+    
+    public func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+            case .kakao:
+                return Observable.create { observer in
+                    Task {
+                        do {
+                            let oauthToken = try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<OAuthToken, Error>) in
+                                if UserApi.isKakaoTalkLoginAvailable() {
+                                    UserApi.shared.loginWithKakaoTalk { (oauthToken, error) in
+                                        if let error = error {
+                                            continuation.resume(throwing: error)
+                                        } else if let oauthToken = oauthToken {
+                                            continuation.resume(returning: oauthToken)
+                                        } else {
+                                            continuation.resume(throwing: NSError(domain: "LoginError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Unknown login error"]))
+                                        }
+                                    }
+                                } else {
+                                    UserApi.shared.loginWithKakaoAccount { (oauthToken, error) in
+                                        if let error = error {
+                                            continuation.resume(throwing: error)
+                                        } else if let oauthToken = oauthToken {
+                                            continuation.resume(returning: oauthToken)
+                                        } else {
+                                            continuation.resume(throwing: NSError(domain: "LoginError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Unknown login error"]))
+                                        }
+                                    }
+                                }
+                            }
+                            let tokens = try await self.authRepository.loginWithKakao(token: oauthToken.accessToken)
+                            KeychainManager.shared.add(key: "accessToken", value: tokens.accessToken)
+                            KeychainManager.shared.add(key: "refreshToken", value: tokens.refreshToken)
+                            observer.onNext(.setLoginStatus(true))
+                        } catch {
+                            observer.onNext(.setLoginStatus(false))
+                            observer.onError(error)
+                        }
+                        observer.onCompleted()
+                    }
+                    
+                    return Disposables.create()
+                }
+            case .apple:
+                return Observable.create { observer in
+                    print("애플로그인")
+//                    let appleIDProvider = ASAuthorizationAppleIDProvider()
+//                    let request = appleIDProvider.createRequest()
+//                    request.requestedScopes = [.fullName, .email]
+//                    
+//                    let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+//                    authorizationController.delegate = self
+//                    authorizationController.presentationContextProvider = self
+//                    authorizationController.performRequests()
+//                    
+                    return Disposables.create()
+                }
+        }
+        
+    }
+    public func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        
+        switch mutation {
+            case .setLoginStatus(let isSuccess):
+                newState.isLoginSuccess = isSuccess
+        }
+        
+        return newState
+    }
+    
+}
--- Projects/Features/Sign/Sources/SignViewController.swift
+++ Projects/Features/Sign/Sources/SignViewController.swift
@@ -6,15 +6,105 @@
 //
 
 import UIKit
-import SnapKit
 import Coordinator
+import ReactorKit
+import RxSwift
+import RxCocoa
+import Coordinator
+import DesignSystem
+import SnapKit
+import YBNetwork
 
-public class SignViewController: UIViewController {
-
-    public weak var coordinator: SignCoordinatorInterface?
+public final class SignViewController: UIViewController, View {
+    public var coordinator: SignCoordinator?
+    public var disposeBag: DisposeBag = DisposeBag()
+    
+    let titleLabel = UILabel()
+    let logoImageView = UIImageView()
+    let illustImageView = UIImageView()
+    let kakaoLoginButton = YBIconWithTextButton(text: "카카오로 시작하기", appearance: .kakao)
+    let appleLoginButton = YBIconWithTextButton(text: "Apple로 시작하기", appearance: .apple)
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        setupViews()
+        setLayouts()
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func bind(reactor: SignReactor) {
+        bindAction(reactor: reactor)
+        bindState(reactor: reactor)
+    }
+    
+    func bindAction(reactor: SignReactor) {
+        kakaoLoginButton.rx.tap
+            .map{ .kakao }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        appleLoginButton.rx.tap
+            .map{ SignReactor.Action.apple}
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+    }
+    
+    func bindState(reactor: SignReactor) {
+        reactor.state.map { $0.isLoginSuccess }
+            .subscribe(onNext : { [weak self] isSuccess in
+                if isSuccess {
+                    self?.coordinator?.createAccount()
+                } else {
+                    //오류 Alert
+                }
+            }).disposed(by: disposeBag)
+    }
+    
+    private func setupViews() {
+        titleLabel.text = "모두를 위한 여행 정산 가계부"
+        titleLabel.font = .ybfont(.title1)
+        view.backgroundColor = YBColor.white.color
+        illustImageView.image = DesignSystemAsset.Icons.illust.image
+        logoImageView.image = DesignSystemAsset.Icons.logo.image
+    }
+    
+    private func setLayouts() {
+        view.addSubview(titleLabel)
+        view.addSubview(logoImageView)
+        view.addSubview(illustImageView)
+        view.addSubview(kakaoLoginButton)
+        view.addSubview(appleLoginButton)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(146)
+            make.centerX.equalToSuperview()
+        }
+        
+        logoImageView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+        }
+        
+        illustImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(logoImageView.snp.bottom).offset(89)
+            make.bottom.equalTo(kakaoLoginButton.snp.top).offset(-49)
+        }
+        
+        kakaoLoginButton.snp.makeConstraints { make in
+            make.top.equalTo(illustImageView.snp.bottom).offset(67)
+            make.centerX.equalTo(view.snp.centerX)
+            make.width.equalTo(view.snp.width).offset(-48)
+        }
+        
+        appleLoginButton.snp.makeConstraints { make in
+            make.top.equalTo(kakaoLoginButton.snp.bottom).offset(20)
+            make.centerX.equalTo(view.snp.centerX)
+            make.width.equalTo(view.snp.width).offset(-48)
+        }
     }
 }
