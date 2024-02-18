5a31d719809e58174e95adb9a9a1d8643e66ae34
taeHyeong Kim <taetae.dev@gmail.com>
2024-02-19 00:51:22 +0900

    feat: 마이프로필 > 닉네임+ 꺽새 버튼 정의

--- Projects/Features/MyPage/Sources/MyPageProfileButton.swift
+++ Projects/Features/MyPage/Sources/MyPageProfileButton.swift
@@ -6,4 +6,43 @@
 //  Copyright © 2024 YeoBee.com. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import DesignSystem
+import SnapKit
+
+final class MyPageProfileButton: UIButton {
+  private var nickname: String
+  private let profileNameLabel = YBLabel(text: "양송이", font: .header2, textColor: .black)
+  private let chevronImageView = UIImageView(image: DesignSystemAsset.Icons.next.image)
+  
+  // MARK: - Init
+  init(frame: CGRect, nickname: String) {
+    self.nickname = nickname
+    super.init(frame: frame)
+    addViews()
+    setLayouts()
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("Not implemented xib init")
+  }
+  
+  // MARK: - Set UI
+  private func addViews() {
+    addSubview(profileNameLabel)
+    addSubview(chevronImageView)
+    
+  }
+  
+  private func setLayouts() {
+    
+    profileNameLabel.snp.makeConstraints { make in
+      make.leading.equalToSuperview()
+    }
+    chevronImageView.snp.makeConstraints { make in
+      make.leading.equalTo(profileNameLabel.snp.trailing).offset(8)
+      make.centerY.equalTo(profileNameLabel.snp.centerY)
+    }
+  }
+}
