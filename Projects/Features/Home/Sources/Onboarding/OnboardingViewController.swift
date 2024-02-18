f66df2bf720b85bd5cc145aa0ea2abebf50de6f5
Jerry_hoyoung <llghdud921@naver.com>
2024-02-19 04:06:53 +0900

    온보딩 개발

--- Projects/Features/Home/Sources/Onboarding/OnboardingViewController.swift
+++ Projects/Features/Home/Sources/Onboarding/OnboardingViewController.swift
@@ -12,96 +12,38 @@ import DesignSystem
 import SnapKit
 import RxCocoa
 
-public final class OnboardingViewController: UIViewController, View {
-    public var disposeBag = DisposeBag()
-    public var coordinator: OnboardingCoordinator?
-    private var pageViewController: UIPageViewController!
-    private var pages: [UIViewController] = []
-    private var currentPageIndex = 0
-    
-    private let nextButton = YBTextButton(text: "", appearance: .default, size: .large)
-    
+public final class OnboardingViewController: UIViewController {
+    let coordinator: OnboardingCoordinator
+
+    // MARK: View
+
+    private let onboardingSheetHostingController: OnboardingSheetHostingController
+
+    public init(
+        coordinator: OnboardingCoordinator
+    ) {
+        self.coordinator = coordinator
+        self.onboardingSheetHostingController = OnboardingSheetHostingController(
+            rootView: .init(
+                store: .init(
+                    initialState: .init(),
+                    reducer: {
+                        OnboardingReducer(coordinator: coordinator)
+                    }
+                )
+            )
+        )
+        super.init()
+    }
+
+
     public override func viewDidLoad() {
         super.viewDidLoad()
-        setupPageViewController()
-        setupNextButton()
         navigationController?.navigationBar.isHidden = true
         navigationController?.interactivePopGestureRecognizer?.isEnabled = false
-        
-        nextButton.rx.tap.bind { [weak self] in
-            guard let self = self else { return }
-            if self.currentPageIndex < self.pages.count - 1 {
-                self.goToNextPage()
-            } else {
-                self.goHome()
-            }
-        }.disposed(by: disposeBag)
     }
     
     required convenience init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupPageViewController() {
-        let page1 = UIViewController()
-        let page2 = UIViewController()
-        let page3 = UIViewController()
-        pages = [page1, page2, page3]
-        page1.view.backgroundColor = .blue
-        page2.view.backgroundColor = .red
-        page3.view.backgroundColor = .yellow
-        
-        pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
-        pageViewController.dataSource = self
-        pageViewController.setViewControllers([pages.first!], direction: .forward, animated: true, completion: nil)
-        
-        addChild(pageViewController)
-        view.addSubview(pageViewController.view)
-        pageViewController.didMove(toParent: self)
-        pageViewController.view.frame = self.view.bounds
-    }
-    
-    private func setupNextButton() {
-        nextButton.setTitle("다음으로", for: .normal)
-        nextButton.backgroundColor = .clear
-        view.addSubview(nextButton)
-        nextButton.snp.makeConstraints { make in
-            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-20)
-            make.centerX.equalTo(view)
-            make.width.equalToSuperview().offset(-24)
-        }
-    }
-    
-    private func goToNextPage() {
-          if currentPageIndex < pages.count - 1 {
-            currentPageIndex += 1
-              let nextViewController = pages[currentPageIndex]
-              pageViewController.setViewControllers([nextViewController], direction: .forward, animated: false, completion: nil)
-          }
-          if currentPageIndex == pages.count - 1 {
-              nextButton.setTitle("시작하기", for: .normal)
-          }
-      }
-    
-    private func goHome() {
-        coordinator?.home()
-    }
-    
-    public func bind(reactor: OnboardingReactor) {
-        //Action
-        
-        //State
-    }
-}
-
-extension OnboardingViewController: UIPageViewControllerDataSource {
-    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        guard let viewControllerIndex = pages.firstIndex(of: viewController), viewControllerIndex > 0 else { return nil }
-        return pages[viewControllerIndex - 1]
-    }
-    
-    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        guard let viewControllerIndex = pages.firstIndex(of: viewController), viewControllerIndex < pages.count - 1 else { return nil }
-        return pages[viewControllerIndex + 1]
     }
 }
---
+++ Projects/Features/Onboarding/Sources/OnboardingView.swift
@@ -0,0 +1,123 @@
+//
+//  OnboardingView.swift
+//  Onboarding
+//
+//  Created Hoyoung Lee on 2/19/24.
+//  Copyright © 2024 YeoBee.com. All rights reserved.
+
+import SwiftUI
+import ComposableArchitecture
+import DesignSystem
+
+
+final class OnboardingSheetHostingController: UIHostingController<OnboardingView> {
+}
+
+public enum OnboardingTab {
+    case register, manage, calculate
+}
+
+struct OnboardingView: View {
+    typealias State = OnboardingReducer.State
+    typealias Action = OnboardingReducer.Action
+
+    let store: StoreOf<OnboardingReducer>
+
+    var body: some View {
+        WithViewStore(store, observe: { $0 }) { viewStore in
+            VStack {
+                TabView(selection: viewStore.$onboadingTab) {
+                    registerView.tag(OnboardingTab.register)
+                    manageView.tag(OnboardingTab.manage)
+                    calculateView.tag(OnboardingTab.calculate)
+                }
+                Button {
+                    viewStore.send(.tappedNextButton(viewStore.onboadingTab))
+                } label: {
+                    Text(viewStore.buttonText)
+                        .foregroundColor(.ybColor(.white))
+                        .font(.ybfont(.title1))
+                        .frame(height: 54)
+                        .frame(maxWidth: .infinity)
+                }
+                .background(YBColor.black.swiftUIColor)
+                .cornerRadius(10)
+                .padding(.horizontal, 24)
+                .padding(.top, 16)
+                .padding(.bottom, 4)
+                .background(YBColor.gray1.swiftUIColor)
+            }
+        }
+    }
+}
+
+extension OnboardingView {
+    var registerView: some View {
+        VStack(spacing: 0) {
+            VStack(spacing: 12) {
+                Text("동행자 등록하기")
+                    .foregroundColor(.ybColor(.black))
+                    .font(.ybfont(.header1))
+                Text("여행 경비를 손쉽게 추적하세요\n최대 10명까지, 각자의 지출을 한눈에!")
+                    .foregroundColor(.ybColor(.black))
+                    .font(.ybfont(.header1))
+                    .multilineTextAlignment(.center)
+            }
+            Spacer()
+            DesignSystemAsset.Icons.travelBee.swiftUIImage
+                .frame(width: 327, height: 219)
+                .padding(.bottom, 40)
+        }
+        .background { YBColor.white.swiftUIColor }
+        .padding(.horizontal, 24)
+        .padding(.top, 96)
+        .frame(height: 450)
+        .cornerRadius(20)
+    }
+
+    var manageView: some View {
+        VStack(spacing: 0) {
+            VStack(spacing: 12) {
+                Text("개인&공동경비 관리하기")
+                    .foregroundColor(.ybColor(.black))
+                    .font(.ybfont(.header1))
+                Text("예산의 특징에 따라 나누어 관리해요\n실시간으로 잔고 내역을 한눈에!")
+                    .foregroundColor(.ybColor(.black))
+                    .font(.ybfont(.header1))
+                    .multilineTextAlignment(.center)
+            }
+            Spacer()
+            DesignSystemAsset.Icons.honeyBee.swiftUIImage
+                .frame(width: 327, height: 199)
+                .padding(.bottom, 40)
+        }
+        .background { YBColor.white.swiftUIColor }
+        .padding(.horizontal, 24)
+        .padding(.top, 96)
+        .frame(height: 450)
+        .cornerRadius(20)
+    }
+
+    var calculateView: some View {
+        VStack(spacing: 0) {
+            VStack(spacing: 12) {
+                Text("여행경비 정산하기")
+                    .foregroundColor(.ybColor(.black))
+                    .font(.ybfont(.header1))
+                Text("간편하게 정산된 금액만 확인해요\n여행의 마지막은 깔끔한 정산!")
+                    .foregroundColor(.ybColor(.black))
+                    .font(.ybfont(.header1))
+                    .multilineTextAlignment(.center)
+            }
+            Spacer()
+            DesignSystemAsset.Icons.calculateBee.swiftUIImage
+                .frame(width: 327, height: 183)
+                .padding(.bottom, 40)
+        }
+        .background { YBColor.white.swiftUIColor }
+        .padding(.horizontal, 24)
+        .padding(.top, 96)
+        .frame(height: 450)
+        .cornerRadius(20)
+    }
+}
