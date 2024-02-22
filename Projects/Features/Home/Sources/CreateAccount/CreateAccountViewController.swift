5f3a7c37216c8ab9fb3890bfd94e0750e63eacd7
taeHyeong Kim <taetae.dev@gmail.com>
2024-02-22 10:45:09 +0900

    feat: 계정생성시 navigation bar back buttom 디자인 수정

--- Projects/Features/Home/Sources/CreateAccount/CreateAccountViewController.swift
+++ Projects/Features/Home/Sources/CreateAccount/CreateAccountViewController.swift
@@ -28,6 +28,17 @@ public final class CreateAccountViewController: UIViewController, View {
         super.viewDidLoad()
         setupViews()
         setLayouts()
+        configureBar()
+    }
+    
+    private func configureBar() {
+        let backImage = UIImage(systemName: "chevron.backward")?.withTintColor(YBColor.gray5.color, renderingMode: .alwaysOriginal)
+        let backButton = UIBarButtonItem(image: backImage, style: .plain, target: self, action: #selector(backButtonTapped))
+        self.navigationItem.leftBarButtonItem = backButton
+    }
+    
+    @objc private func backButtonTapped() {
+        self.navigationController?.popViewController(animated: true)
     }
     
     required convenience init?(coder aDecoder: NSCoder) {
