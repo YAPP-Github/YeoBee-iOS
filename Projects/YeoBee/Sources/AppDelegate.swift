33c21963d25a3045ad7227ddc4a577d628cc4c77
Jerry_hoyoung <llghdud921@naver.com>
2024-02-22 11:55:47 +0900

    카카오 로그인 안되는것 해결

--- Projects/YeoBee/Sources/AppDelegate.swift
+++ Projects/YeoBee/Sources/AppDelegate.swift
@@ -23,9 +23,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        if (AuthApi.isKakaoTalkLoginUrl(url)) {
-            return AuthController.handleOpenUrl(url: url)
-        }
         return false
     }
 }
